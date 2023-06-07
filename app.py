1ab357bd8fdab75c614f5d97ffdeb96755205ccd
Kirti Subramanian <68790244+kirtisubs06@users.noreply.github.com>
2023-06-07 11:45:20 -0700

    Add files via upload
    added introduction text to the app

--- app.py
+++ app.py
@@ -36,6 +36,25 @@ def homepage():
 
 def show_app_title_and_introduction():
     st.title("Welcome to LearnSmart!")
+    st.write(f"""
+            LearnSmart is a tool to enhance your English and Math skills!
+
+            It is an interactive app that helps you learn and improve your proficiency in English and Math. 
+            Dive into engaging lessons, practice exercises, and quizzes to strengthen your knowledge and 
+            understanding of these subjects.
+
+            Whether you're a student looking to excel in school or someone wanting to sharpen your skills, 
+            LearnSmart provides a comprehensive learning experience. From grammar and vocabulary to 
+            problem-solving and mathematical concepts, explore a wide range of topics and level up your 
+            abilities.
+
+            With interactive exercises and real-time feedback, you can track your progress and identify areas 
+            for improvement. LearnSmart is your go-to tool for mastering English and Math in an enjoyable and 
+            effective way. Join hundreds of learners who have already completed 0 
+            sessions and conquered 0 challenges on their learning journey.
+
+            Start your learning journey with LearnSmart today and unlock your full potential in English and Math!
+        """)
 
 
 if __name__ == "__main__":
